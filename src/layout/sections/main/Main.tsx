49eaa7d604bfb614f958b2776c84c9471312ba64
alexpodelinskii <nikitik00787@gmail.com>
2025-08-27 17:58:32 +0300

    feat: add main section styles

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,31 +1,68 @@
 import photo from './../../../assets/images/Photo.jpg'
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Container} from "../../../components/Container.tsx";
+import {theme} from "../../../styles/Theme.ts";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align='center' justify={'space-around'}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer. </MainTitle>
-                </div>
+            <Container>
+                <FlexWrapper align='center' justify={'space-between'}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>I am <span>Svetlana Dyablo</span></Name>
+                        <MainTitle>A Web Developer. </MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt='My photo'/>
+                    </PhotoWrapper>
 
-                <Photo src={photo} alt='My photo'/>
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
+
         </StyledMain>
 
     );
 };
 const StyledMain = styled.section`
     min-height: 100vh;
-    background-color: aqua;
+    display: flex;
 `
-const MainTitle = styled.h1`
 
+const SmallText = styled.h2`
+    font-size: 14px;
+    font-weight: 400;
+    text-align: left;
 `
 const Name = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 50px;
+    text-align: left;
+
+    span {
+        position: relative;
+        z-index: 2;
+
+        &::before {
+            content: '';
+            display: block;
+            height: 20px;
+            width: 100%;
+            position: absolute;
+            bottom: 0px;
+            left: 0;
+            background-color: ${theme.colors.fontColorAccent};
+            z-index: -1;
+        }
+
+    }
+`
+const MainTitle = styled.h1`
+    font-weight: 400;
+    font-size: 27px;
+    text-align: left;
 
 `
 
@@ -33,4 +70,22 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    
+`
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 2;
+    &::before{
+        content: '';
+        display: block;
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.fontColorAccent};
+        position: absolute;
+        left: 24px;
+        bottom: 24px;
+        z-index: -1;
+    }
+    
+
 `
---
+++ src/styles/Clobal.styled.ts
@@ -0,0 +1,104 @@
+import {createGlobalStyle} from "styled-components";
+import {theme} from "./Theme.ts";
+
+export const GlobalStyled = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    a,
+    a:visited {
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-weight: inherit;
+        font-size: inherit;
+    }
+
+    img {
+        vertical-align: top;
+    }
+
+    :focus, :active {
+        outline: none;
+    }
+
+    a:focus, a:active {
+        outline: none;
+    }
+
+    nav, footer, header, aside {
+        display: block;
+    }
+
+    html, body {
+        height: 100%;
+        width: 100%;
+        font-size: 100%;
+        line-height: 1;
+        font-size: 14px;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-text-size-adjust: 100%;
+        color: ${        theme.colors.fontsColor    };
+    }
+
+    input, button, textarea {
+        font-family: inherit;
+    }
+
+    input::-ms-clear {
+        display: none;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    button::-moz-focus-inner {
+        padding: 0;
+        border: 0;
+    }
+
+
+    body {
+
+        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        line-height: 1.2;
+    }
+
+    section:nth-of-type(odd) {
+        background-color: ${theme.colors.primaryBg};
+    }
+
+    section:nth-of-type(even) {
+        background-color: ${theme.colors.secondaryBg};
+    }
+
+
+`
